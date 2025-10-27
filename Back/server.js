d4c18c7680b6d5b8b1cff7cbe124b95cd2bbe8c9
M_Ibrokhiiim <ibrokhimmuysinov@gmail.com>
2025-10-27 23:33:49 +0500

    Back

--- Back/server.js
+++ Back/server.js
@@ -2,9 +2,12 @@ import express from 'express'
 import dotenv from 'dotenv'
 import todoRouter from './routers/taskOperations.js'
 import ErrorHandler from './middleWare/errorHandler.js'
+import cors from "cors";
+
 
 const app = express()
 dotenv.config()
+app.use(cors());
 
 app.use(express.json())
 app.use(express.urlencoded({extended:false}))
